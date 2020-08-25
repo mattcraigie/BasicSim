02574183482a6b2bf32563aa01da6e3ed5743822
mattcraigie <matthewcraigie1@gmail.com>
2020-08-25 17:45:04 +1000

    Included Sphinx

--- simulation.py
+++ simulation.py
@@ -1,49 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation, FFMpegWriter
-from scipy.fft import fftn, ifftn, rfftn, irfftn
+from scipy.fft import rfftn, irfftn
 import matplotlib as mpl
 mpl.rcParams['animation.ffmpeg_path'] = r'C:\\Program Files (x86)\\FFMpeg\\bin\\ffmpeg.exe'
-
-np.random.seed(42)
-
-# Sim setup
-n_particles = 15**3
-n_frames = 10
-frame_duration = 100
-
-n_grid_cells = 30
-
-L_box = 1
-H_0 = 1
-G = 1
-Omega_m0 = 1
-mass_particle = 1
-Omega_0 = 1
-
-r_0 = L_box / n_grid_cells
-t_0 = 1 / H_0
-rho_0 = 3 * H_0**2 * Omega_m0 / (8 * np.pi * G)
-v_0 = r_0 / t_0
-phi_0 = v_0 ** 2
-
-
-# Set initial positions and velocities
-position = n_grid_cells * np.random.random((3, n_particles))
-
-x = np.linspace(0, 1, 15)
-position = np.array([[i, j, k] for i in x for j in x for k in x]).transpose() * n_grid_cells
-velocity = 0 * (0.5 - np.random.random((3, 15**3)))
-
-# position = np.array([[25., 35.], [0., 0.], [0., 0.]])
-# velocity = np.array([[0., 0.], [10., 10.], [0., 0.]])
-
-# frame counter
-frame = 0
-
-#
-a = 0.1
-da = 1e-2
 
 
 def apply_boundary(p):
@@ -299,14 +259,63 @@ def update(i):
     return points_sim,
 
 
-def run_sim_pm(filename, n_particles, n_grid, n_frames, show_ps=True, ps_bins=50):
-    return
+def run_sim_pm(filename, n_particles=1000, n_grid=30, n_frames=1000, show_ps=False, ps_bins=50):
+    """
+    Creates and runs a simulation using a particle-mesh N-body implementation. An mp4 file of the simulation is saved at
+    the location specified by filename. The particle-mesh method scales with O(N + G*log[G]), and operations are done in
+    place so large number of particles can be computed in reasonable times. Setting show_ps=True will slow computation
+    but is also calculated with O(G*log[G]).
 
+    :param filename: name of the file to save the simulation video
+    :param n_particles: the number of particles to use in the simulation.
+    :param n_grid: number of grid cells along a single dimension, total grid cells will be n_grid**3
+    :param n_frames: number of frames to run the simulation for
+    :param show_ps: set to true to plot the power spectrum (k * P[k]) and correlation function (xi[r]) alongside the
+    simulation
+    :param ps_bins: the number of bins to use for the power spectrum and correlation function if show_ps=True
+    :return: returns None
+    """
 
+    np.random.seed(42)
 
-if __name__ == '__main__':
+    # Sim setup
+    n_particles = 15 ** 3
+    n_frames = 10
+    frame_duration = 100
 
-    plot_powerspectrum = True
+    n_grid_cells = 30
+
+    L_box = 1
+    H_0 = 1
+    G = 1
+    Omega_m0 = 1
+    mass_particle = 1
+    Omega_0 = 1
+
+    r_0 = L_box / n_grid_cells
+    t_0 = 1 / H_0
+    rho_0 = 3 * H_0 ** 2 * Omega_m0 / (8 * np.pi * G)
+    v_0 = r_0 / t_0
+    phi_0 = v_0 ** 2
+
+    # Set initial positions and velocities
+    position = n_grid_cells * np.random.random((3, n_particles))
+
+    x = np.linspace(0, 1, 15)
+    position = np.array([[i, j, k] for i in x for j in x for k in x]).transpose() * n_grid_cells
+    velocity = 0 * (0.5 - np.random.random((3, 15 ** 3)))
+
+    # position = np.array([[25., 35.], [0., 0.], [0., 0.]])
+    # velocity = np.array([[0., 0.], [10., 10.], [0., 0.]])
+
+    # frame counter
+    frame = 0
+
+    #
+    a = 0.1
+    da = 1e-2
+
+    plot_powerspectrum = show_ps
 
     x = np.arange(0, n_grid_cells)
     pos_grid = np.array(np.meshgrid(x, x, x))
@@ -318,8 +327,6 @@ if __name__ == '__main__':
     k_norm = k_norm[:, :, :half]
 
     greens = greens_function(k_grid)
-
-
 
     if plot_powerspectrum:
         alpha = 100
@@ -359,14 +366,15 @@ if __name__ == '__main__':
         bin_edges = np.linspace(np.min(k_norm), np.max(k_norm), n_bins + 1)
         bin_width = bin_edges[1] - bin_edges[0]
         bin_mids = (bin_edges + bin_width)[:-1]
-        powspec = ax2.bar(bin_mids, np.zeros(len(bin_mids)), align='center', width=9/n_bins, color='white',
-                           edgecolor='royalblue')
+        powspec = ax2.bar(bin_mids, np.zeros(len(bin_mids)), align='center', width=9 / n_bins, color='white',
+                          edgecolor='royalblue')
 
         n_bins = 60
         bin_edges = np.linspace(0, n_grid_cells * np.sqrt(3), n_bins + 1)
         bin_width = bin_edges[1] - bin_edges[0]
         bin_mids = (bin_edges + bin_width)[:-1]
-        corrfunc = ax3.bar(bin_mids, np.zeros(len(bin_mids)), align='center', width=(n_grid_cells * np.sqrt(3))/n_bins, color='white', edgecolor='royalblue')
+        corrfunc = ax3.bar(bin_mids, np.zeros(len(bin_mids)), align='center',
+                           width=(n_grid_cells * np.sqrt(3)) / n_bins, color='white', edgecolor='royalblue')
 
         ani = FuncAnimation(fig, update, frames=n_frames)
         f = r"sim_pm.mp4"
@@ -374,7 +382,6 @@ if __name__ == '__main__':
         ani.save(f, writer=writervideo)
 
     if not plot_powerspectrum:
-
 
         # Set the axes on which the points will be shown
         plt.ion()  # Set interactive mode on
@@ -385,9 +392,22 @@ if __name__ == '__main__':
         # Create command which will plot the positions of the particles
         points_sim, = plt.plot([], [], 'o', markersize=1)
 
-        ani = FuncAnimation(fig, update, frames=n_frames)# interval=frame_duration)
+        ani = FuncAnimation(fig, update, frames=n_frames)  # interval=frame_duration)
         writervideo = FFMpegWriter(fps=60)
+
+        if filename[:-3] != '.mp4':
+            f = filename + '.mp4'
+        else:
+            f = filename
+
         ani.save(filename, writer=writervideo)
+
+    return None
+
+
+if __name__ == '__main__':
+    print('Do not run this file. Import using >> from simulation import run_sim_pm')
+
 
 
 
